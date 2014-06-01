347bba38d95bf6020f144c01876a094ade54aac7
jasonparker <jason.parker@gmail.com>
2014-06-01 16:27:50 -0400

    adding functions file to child theme

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+//declare the name of child theme
+$themename = "Wagner-Murray";
+
+// enqueue styles
+if( !function_exists("theme_styles") ) {  
+    function theme_styles() { 
+        // This is the compiled css file from LESS - this means you compile the LESS file locally and put it in the appropriate directory if you want to make any changes to the master bootstrap.css.
+        wp_register_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css', array(), '1.0', 'all' );
+        wp_enqueue_style( 'bootstrap' );
+
+        // For child themes
+        wp_register_style( 'wpbs-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all' );
+        wp_enqueue_style( 'wpbs-style' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'theme_styles' );
+
+?>
