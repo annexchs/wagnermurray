bfa7d8c407682da28c8a0e4c811283009c3335fd
bwoods89 <bwoods89@gmail.com>
2014-06-01 15:37:32 -0400

    Added header file

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>
+    Wagner Murray Architects, P.A. - Charlotte, NC
+  </title><!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="css/style.css" rel="stylesheet" type="text/css"><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+      	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+       <![endif]-->
+       <?php wp_head(); ?>
+     </head>
+     <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2 header logo">
+            <img class="img-responsive" src="http://placehold.it/500x500" alt="placeholder">
+          </div>
+          <div class="col-md-2 header blank">
+          </div>
+          <div class="col-md-2 header navi">
+            <ul class="list-unstyled">
+              <li><a href="#">PROJECTS</a></li>
+              <li><a href="#">ABOUT</a></li>
+              <li><a href="#">BLOG</a></li>
+              <li><a href="#">CONTACT</a></li></ul>
+            </div>
+            <div class="col-md-2 header tease">
+              <a href="#"><h5><strong>LATEST FROM THE BLOG</strong></h5></a>
+              <a href="#"><h6 class="homeheadline"><em>Headline</em></h6></a>
+              <a href="#"><div class="date">POSTED JUNE 2, 2014</div></a>
+              <a href="#"><p>Preview text lorem ipsum
+                dolor sit amet</p></a>
+            </div>
+            <div class="col-md-2 header address">
+              <address><strong>Wagner Murray Archictects, P.A.</strong>
+                601 S. Cedar St., Studio 101
+                Charlotte, NC 28202
+                (704) 555-5555</address>
+            </div>
+          </div>
