7795c1dde25ac99293c496bfb424e1d0609385fe
bwoods89 <bwoods89@gmail.com>
2014-06-02 18:34:13 -0400

    Added the wordpress bar for logged-in users

--- footer.php
+++ footer.php
@@ -4,5 +4,6 @@
           <p class="pull-left">&copy; 2014 <?php bloginfo('name'); ?></p>
           <a class="pull-right" href="http://www.annexstudio.com" target="_blank"><img src="wp-content/themes/wagner-murray/images/built-by-annex-bumper.png"></a>
         </div>
+        <?php wp_footer(); ?>
       </body>
     </html>
