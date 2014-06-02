7153d1215b9e344024efed055aa03a6756f0afde
yost <geoff@annexstudio.com>
2014-06-02 02:09:13 -0400

    footer fix 2

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         </div>
         <div class="container">
           <hr>
-          <p class="pull-left">&copy; 2014 <?php bloginfo('name'); ?>
+          <p class="pull-left">&copy; 2014 <?php bloginfo('name'); ?></p>
           <a class="pull-right" href="http://www.annexstudio.com"><img src="wp-content/themes/wagner-murray/images/built-by-annex-bumper.png"></a>
         </div>
       </body>
