72b8a77ea942ede7cc0baa3a5747eb6558aee85b
jasonparker <jason.parker@gmail.com>
2014-06-02 01:49:02 -0400

    added homepage page template

---
+++ page-homepage.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+      <div class="row pageheader">
+        <div class="col-md-12">
+
+          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+          <h3 class="uppercase h4fix"><?php the_title(); ?></h3>
+        </div>
+      </div>
+      <div class="row category-row" id="public-spaces">
+        <?php the_content(); ?>
+      </div>
+    <?php endwhile; ?>
+  <?php endif; ?>
+
+<?php get_footer(); ?>
