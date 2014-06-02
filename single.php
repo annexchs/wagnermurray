7b867afaab158bb6f449066651a24a9f2cdfd21f
bwoods89 <bwoods89@gmail.com>
2014-06-02 02:00:42 -0400

    Finished single.php

--- single.php
+++ single.php
@@ -5,16 +5,24 @@
   <div class="row pageheader">
     <div class="col-md-10"> 
 
-      <h6 class="uppercase">
-      <?php $catCount = 0; foreach((get_the_Category()) as $category) ?>
+      <style>
+        .uppercase .header:last-child {
+          display:none;
+        }
+      </style>
+      <h6 class="uppercase" >
+      <?php $catCount = 0; foreach((get_the_Category()) as $category) { ?>
         <span class="<? echo $category->slug ?>"><?php echo $category->name ?></span>
+      <?php $catCount += 1; ?>
+        <span class="header">&nbsp;|&nbsp;</span>
+      <?php }; ?>
       </h6>
 
     </div>
   </div>
   <div class="row category-row" id="public-spaces projects">
     <div class="col-md-10 featured-project bottomfix">
-      <div id="project-images" class="carousel slide" data-ride="carousel">
+      <div id="project-images" class="carousel slide" data-ride="carousel" data-interval="5000">
         <ol class="carousel-indicators">
 
           <?php
@@ -45,31 +53,34 @@
         </ol> -->
 
           <!-- Wrapper for slides -->
+
         <div class="carousel-inner">
+          <?php $imgCount=0; foreach($images as $image) { 
+            if ($imgCount == 0) {
+          ?>
           <div class="item active">
-            <img src="http://placehold.it/1500x750" alt="placeholder">
+            <img src="<?php echo wp_get_attachment_image_src( $image->ID, 'full' )[0]?>" alt="<?php echo get_post_meta($image->ID, '_wp_attachment_image_alt', true); ?>">
           </div>
+          <?php } else { ?>
           <div class="item">
-            <img src="http://placehold.it/1500x750" alt="placeholder">
+            <img src="<?php echo wp_get_attachment_image_src( $image->ID, 'full' )[0]?>" alt="<?php echo get_post_meta($image->ID, '_wp_attachment_image_alt', true); ?>">
           </div>
-          <div class="item">
-            <img src="http://placehold.it/1500x750" alt="placeholder">
-          </div>
+          <?php } $imgCount+=1; }; ?>
         </div>
 
         <!-- Controls -->
         <a class="left carousel-control" href="#project-images" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left public-spaces"></span>
+          <span class="glyphicon glyphicon-chevron-left"></span>
         </a>
         <a class="right carousel-control" href="#project-images" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right public-spaces"></span>
+          <span class="glyphicon glyphicon-chevron-right"></span>
         </a>
       </div>
     </div>
 </div>
   <div class="row pageheader">
     <div class="col-md-3">
-      <h3 class="retail"><?php the_title(); ?></h3>
+      <h3 class="<?php echo get_the_category()[0]->slug ?>"><?php the_title(); ?></h3>
       <h4><?php $clientKey="Client"; echo get_post_custom_values($clientKey)[0]; ?></h4>
       <h4><?php $locationKey="Location"; echo get_post_custom_values($locationKey)[0]; ?></h4>
     </div>
