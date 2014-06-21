bbb63d28b505c48fa31103987050e1c939a168ce
jasonparker <jason.parker@gmail.com>
2014-06-20 20:38:35 -0400

    Fixed header for mobile

--- header.php
+++ header.php
@@ -25,13 +25,14 @@
  <body>
   <div class="container">
     <div class="row">
-      <div class="col-xs-2 header logo">
-        <a href="<?php echo site_url(); ?>"><img class="img-responsive" src="<?php echo site_url(); ?>/wp-content/themes/wagner-murray/images/logo.png" alt="Wagner Murray Architects"></a>
+      <div class="col-xs-8 col-xs-offset-1 col-sm-offset-0 col-sm-2 header logo">
+        <a href="<?php echo site_url(); ?>"><img class="img-responsive site-logo" src="<?php echo site_url(); ?>/wp-content/themes/wagner-murray/images/logo.png" alt="Wagner Murray Architects"></a>
       </div>
-      <div class="col-xs-2 header blank">
+      <div class="col-xs-1 col-sm-2 header blank">
       </div>
-      <div class="col-xs-2 header navi">
-        <ul class="list-unstyled">
+      <div class="col-xs-10 col-sm-2 header navi">
+        <hr class="visible-xs">
+        <ul class="list-unstyled menu-nav-list">
           <li><a href="?page_id=194">PROJECTS</a></li>
           <li><a href="?page_id=120">ABOUT</a></li>
           <li><a href="?cat=2">BLOG</a></li>
@@ -41,14 +42,14 @@
       $headerPost = new WP_Query();
       $headerPost->query('showposts=1&cat=2');
       while ($headerPost->have_posts()) : $headerPost->the_post(); ?>
-      <div class="col-md-2 header tease hidden-xs hidden-sm">
+      <div class="col-md-2 header tease hidden-xs">
         <a href="<?php the_permalink(); ?>"><h5><strong>LATEST FROM THE BLOG</strong></h5></a>
         <a href="<?php the_permalink(); ?>"><h6 class="homeheadline"><em><?php the_title(); ?></em></h6></a>
         <div class="date">POSTED <?php the_date() ?></div>
         <a href="<?php the_permalink(); ?>"><p><?php the_excerpt(); ?></p></a>
       </div>
       <?php endwhile; ?>
-      <div class="col-xs-2 header address">
+      <div class="col-xs-2 header address visible-md visible-lg">
         <address><strong>Wagner Murray Archictects, P.A.</strong>
           601 S. Cedar St., Studio 101
           Charlotte, NC 28202<br>
