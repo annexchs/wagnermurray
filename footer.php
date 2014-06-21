1cc3f66a16d4d80978f3b8e4edca199e5a0b8c74
jasonparker <jason.parker@gmail.com>
2014-06-20 20:38:01 -0400

    Fixed relative src

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <div class="container">
           <hr>
           <p class="pull-left">&copy; 2014 <?php bloginfo('name'); ?></p>
-          <a class="pull-right" href="http://www.annexstudio.com" target="_blank"><img src="wp-content/themes/wagner-murray/images/built-by-annex-bumper.png"></a>
+          <a class="pull-right" href="http://www.annexstudio.com" target="_blank"><img src="/wp-content/themes/wagner-murray/images/built-by-annex-bumper.png"></a>
         </div>
         <?php wp_footer(); ?>
 
